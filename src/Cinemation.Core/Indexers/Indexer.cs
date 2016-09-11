18855eada27b590ac636ecb0f69edbc2b530800b
AeonLucid <aeonlucid@outlook.com>
2016-09-11 17:27:23 +0200

    Start on indexer.

---
+++ src/Cinemation.Core/Indexers/Indexer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cinemation.Core.Indexers
+{
+    internal abstract class Indexer
+    {
+
+        /// <summary>
+        /// Gets the base <see cref="Uri"/>.
+        /// </summary>
+        /// <returns>Returns the base <see cref="Uri"/>.</returns>
+        public abstract Uri GetBaseUri();
+
+    }
+}
---
+++ src/Cinemation.Core/Indexers/RarBgIndexer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cinemation.Core.Indexers
+{
+    internal class RarBgIndexer : Indexer
+    {
+        private static readonly Uri BaseUri = new Uri("https://torrentapi.org/pubapi_v2.php");
+
+        public override Uri GetBaseUri()
+        {
+            return BaseUri;
+        }
+    }
+}
