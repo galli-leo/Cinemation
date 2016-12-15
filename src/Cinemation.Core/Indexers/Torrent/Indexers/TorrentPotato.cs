8b4ed27ff959c19021531e829eb8d1b7341db754
Leonardo Galli <leonardo.galli@bluewin.ch>
2016-12-15 12:47:41 +0100

    First implementation of TorrentPotato provider
    Also added Score & DownloadURL to TorrentData

---
+++ src/Cinemation.Core/Indexers/Torrent/Indexers/TorrentPotato.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Cinemation.Core.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Cinemation.Core.Indexers.Torrent.Indexers
+{
+    public class TorrentPotatoIndexer : TorrentIndexer
+    {
+        private string EndpointMovies = "http://127.0.0.1:9117/potato/privatehd";
+        private string APIKey = "vu5gdo460fvooqhlf58nqvjxmgaxma8v";
+
+        public TorrentPotatoIndexer() : base("TorrentPotato")
+        {
+        }
+
+        protected override void Configure(IndexerConfigurator config)
+        {
+            
+        }
+
+        protected override async Task<List<TorrentData>> PerformSearchAsync(SearchData searchData)
+        {
+            var queryTerm = searchData.HasImdbCode ? $"imdbid={ searchData.ImdbCode}" : $"search={searchData.MovieName}";
+
+            var torrentsData = new List<TorrentData>();
+            var response = await HttpClient.GetStringAsync($"{EndpointMovies}?passkey={APIKey}&{queryTerm}");
+
+            var jToken = JsonConvert.DeserializeObject<JToken>(response);
+            var movies = jToken.Value<JToken>("results");
+
+            if (movies == null) return torrentsData;
+
+            foreach (var movie in movies)
+            {
+                var downloadURL = movie.Value<JToken>("download_url");
+                if (downloadURL == null) continue;
+
+                torrentsData.Add( new TorrentData
+                        {
+                            Title = new TorrentTitle
+                            {
+                                MovieTitle = movie.Value<string>("release_name"),
+                                MovieYear = 1999,
+                                VideoResolution = "",
+                                VideoSource = "BluRay",
+                                AudioChannel = "2.0",
+                                AudioCodec = "AAC",
+                                Group = "YTS"
+                            }.GetTitle(),
+                            Magnet = downloadURL.ToString(),
+                            Seeds = movie.Value<int>("seeders"),
+                            Peers = movie.Value<int>("leechers"),
+                            Size = movie.Value<long>("size"),
+                        }
+                );
+            }
+
+            return torrentsData;
+        }
+
+        private static string GetMagnetUri(string hash, string displayName)
+        {
+            if(string.IsNullOrEmpty(hash))
+                throw new NullReferenceException("hash can't be null.");
+
+            if(string.IsNullOrEmpty(displayName))
+                throw new NullReferenceException("displayName can't be null.");
+
+            return $"magnet:?xt=urn:btih:{hash}&dn={WebUtility.UrlEncode(displayName)}&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A2750%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce";
+        }
+    }
+}
--- src/Cinemation.Core/Indexers/Torrent/TorrentData.cs
+++ src/Cinemation.Core/Indexers/Torrent/TorrentData.cs
@@ -13,6 +13,14 @@
         /// </summary>
         public string Magnet { get; set; }
 
+        
+        public bool HasMagnet => !string.IsNullOrEmpty(Magnet);
+
+        /// <summary>
+        ///     The download url to torrent file.
+        /// </summary>
+        public string DownloadULR { get; set; }
+
         /// <summary>
         ///     Torrent seeders count.
         /// </summary>
@@ -27,6 +35,12 @@
         ///     Torrent size in bytes.
         /// </summary>
         public long Size { get; set; }
+
+        /// <summary>
+        ///     The score of the torrent. The higher the score, the more likely the torrent will get chosen.
+        ///     TODO: Determine parameters which decide score.
+        /// </summary>
+        public long Score { get; set; }
 
         /// <summary>
         ///     MetaData about the 
